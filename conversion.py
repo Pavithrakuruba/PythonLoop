2b106f5302f3f01fb539ee695d64bdc3ee2acd72
Pavithrakuruba <103957063+Pavithrakuruba@users.noreply.github.com>
2022-11-12 16:29:12 +0530

    Add files via upload

---
+++ conversion.py
@@ -0,0 +1,121 @@
+###*****************************************complex to int
+# a=20+4j
+# print(int(a))
+
+# a=20+5j
+# b=2
+# c=a+b
+# print(type(c))
+
+###Complex to float
+# a=20+4j
+# print(float(a))
+
+# a=20+4j
+# b=2.5
+# print(a+b)
+
+#### complex to str
+# a=20+4j
+# print(str(a))
+
+# a=20+4j
+# b="pavi"
+# print(a+b)
+
+# a=20+4j
+# b="45"
+# print(a+b)
+
+
+# a="pavi"
+# b=45
+# c=45.9
+# d=45+5j
+# f=(a,b,c+d)
+# print(a)
+# print(type(a))
+# print(f)
+# print(type(f))
+
+###************************************str to int
+# a="45"
+# print(int(a))
+
+# a="45"
+# b=45
+# print(a+b)
+
+
+### str to float
+# a="45"
+# print(float(a))
+
+# a="45"
+# b=45.7
+# print(a+b)
+
+###str to cmplex
+# a="45"
+# print(complex(a))
+
+# a="45"
+# b=45+6j
+# print(a+b)
+
+# a="45"
+# print(type(a)==int)
+# b=type(a)==str
+# print(b)
+
+####***************************************int to str
+# a=45
+# print(str(a))
+
+# a=45
+# b="Pavi"
+# print(a+b)
+
+# ####int to float
+# a=45
+# print(float(a))
+
+# a=45
+# b=45.6
+# print(a+b)
+
+####int to complex
+# a=45
+# b=(complex(a))
+# print(type(b))
+
+# a=45
+# b=10+7j
+# print(a+b)
+
+####**********************************float to int
+# a=45.9
+# print(int(a))
+
+# a=45.9
+# b=45
+# print(a+b)
+
+###float to str
+# a=45.9
+# print(str(a))
+
+# a=45.9
+# b="45"
+# print(a+b)
+
+
+####float to complex
+# a=45.9
+# print(complex(a))
+
+# a=45.9
+# b=45+9j
+# print(a+b)
+
+
